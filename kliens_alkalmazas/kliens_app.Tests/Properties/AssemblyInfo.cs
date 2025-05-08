c79c3fec933870c9bfc97eefdfaba60fdb16d2a5
GoofyG00ber <czanik.csanad@gmail.com>
2025-05-08 23:12:44 +0200

    sok szenvedés után, de lett unit tesztelés. Próbáltam többet tesztelni miután ez a teszt meg lett, de nem sikerült egyenlőre.

---
+++ kliens_alkalmazas/kliens_app.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("kliens_app.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kliens_app.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("193b549f-a63b-4a55-8239-b5f8c381760f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ kliens_alkalmazas/kliens_app.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kliens_alkalmazas;
+using Hotcakes.CommerceDTO.v1.Client;
+
+namespace kliens_app.Tests
+{
+    [TestClass]
+    public class UserControlKeszletTests
+    {
+        [TestMethod]
+        public void Raktar_API_hivas_test()
+        {
+            // Act
+            var api = typeof(UserControlKeszlet)
+                      .GetMethod("apiHivas", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+                      .Invoke(null, null) as Api;
+
+            // Assert
+            Assert.IsNotNull(api);
+        }
+    }
+}
